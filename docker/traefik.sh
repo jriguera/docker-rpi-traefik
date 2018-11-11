8f3b1fcc88f7fca7c0b65258e3020ecfa0354edd
Jose Riguera <jriguera@gmail.com>
2018-11-11 02:00:14 +0100

    Initial version

---
+++ docker/traefik.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eo pipefail
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+    set -- traefik "$@"
+fi
+
+# if our command is a valid Traefik subcommand, let's invoke it through Traefik instead
+# (this allows for "docker run traefik version", etc)
+if traefik "$1" --help | grep -s -q "help"; then
+    set -- traefik "$@"
+fi
+
+exec "$@"
+
